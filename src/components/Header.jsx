95c06f29d2db4aaf2a87c5806789082e9b138e23
Kevin Ruangrad <kevin_9@live.se>
2025-01-31 16:10:08 +0100

    First Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { HashLink as Link } from "react-router-hash-link";
+
+const Header = () => {
+  return (
+    <header className="flex relative justify-between font-display text-white font-semibold p-6 px-8 mt-4 mx-2 bg-[#131315] rounded-full">
+      <h1>kevinruangrad.</h1>
+      <nav>
+        <ul className="flex space-x-10">
+          <li>
+            <Link to="#work">work.</Link>
+          </li>
+          <li>
+            <Link to="#spotify">contact.</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
