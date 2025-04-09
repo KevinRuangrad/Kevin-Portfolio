5dfe50e40aaf065828b6c7f54028527a4375f086
Kevin Ruangrad <kevin_9@live.se>
2025-04-09 12:55:40 +0200

    work update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui/package.json" assert { type: "json" };
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        "sans": ["ui-sans-serif", "system-ui"],
-        "serif": ["ui-serif", "Georgia"],
-        "mono": ["ui-monospace", "SFMono-Regular"],
-        "display": ["Inter Display", "ui-sans-serif"],
-        "body": ['""'],
+        sans: ["ui-sans-serif", "system-ui"],
+        serif: ["ui-serif", "Georgia"],
+        mono: ["ui-monospace", "SFMono-Regular"],
+        display: ["Inter Display", "ui-sans-serif"],
+        body: ['""'],
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
 };
